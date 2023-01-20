c619cf881205d7d90d260bde23c77413a095ab17
Catherine Kim <ckim722@gmail.com>
2023-01-19 20:49:44 -0500

    updates files

---
+++ index.js
@@ -0,0 +1,31 @@
+const { spyOn } = require("expect/lib");
+
+function shout(string){
+    return string.toUpperCase(); 
+}
+function whisper(string){
+    return string.toLowerCase(
+
+    )
+}
+function logShout(string){
+    
+    console.log('HELLO');
+}
+function logWhisper(string){
+    
+    console.log('hello');
+}
+
+function sayHiToHeadphonedRoommate(string){
+    
+    if(string === string.toLowerCase() ){
+        return "I can\'t hear you!"; 
+    }
+    if(string === string.toUpperCase()){
+        return "YES INDEED!";
+    }
+    else if(string === "Let\'s have dinner together!"){
+        return "I would love to!";
+    }
+}
